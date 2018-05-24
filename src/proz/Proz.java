f6af167840478a517ee5813c3b0f76b9c0150ab4
z3nd0g <z3nd0g@gmail.com>
2018-05-24 15:33:56 +0200

    Revert "1st commit"
    This reverts commit 00a923a8909892ecdda4194eca857c5bea9cc7b9.

--- src/proz/Proz.java
+++ src/proz/Proz.java
@@ -1,30 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package proz;
 
-import java.awt.EventQueue;
-import javax.swing.JFrame;
 /**
  *
  * @author z3nd0g
  */
-public class Proz extends JFrame{
-    
-    public Proz() {
+public class Proz {
 
-        add(new Board());
-
-        setResizable(false);
-        pack();
-        
-        setTitle("LIFE");    
-        setLocationRelativeTo(null); //places the frame in the centre
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
-    }  
-
+    /**
+     * @param args the command line arguments
+     */
     public static void main(String[] args) {
-        
-        EventQueue.invokeLater(() -> {
-            JFrame proz = new Proz();
-            proz.setVisible(true);
-        });
-    }   
+        // TODO code application logic here
+    }
+    
 }
